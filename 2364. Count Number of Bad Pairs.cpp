95c3d9f63e11aa77273f68a7a11f9994e369ea12
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-02-09 15:37:32 +0530

    Update 2364. Count Number of Bad Pairs.cpp

--- 2364. Count Number of Bad Pairs.cpp
+++ 2364. Count Number of Bad Pairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
         long long n = nums.size(), goodPairs = 0;
-        map<long, long> posVal;
+        unordered_map<long, long> posVal;
         for (int i = 0; i < n; i++) {
             int key = i - nums[i];
             goodPairs += posVal[key]++;
@@ -10,3 +10,15 @@ public:
         return (n * (n - 1) / 2) - goodPairs;
     }
 };
+
+// class Solution {
+// public:
+//     long long countBadPairs(vector<int>& nums) {
+//         long long n = nums.size(), goodPairs = 0;
+//         unordered_map<long, long> posVal;
+//         for (int i = 0; i < n; i++)
+//             goodPairs += posVal[i - nums[i]]++;
+
+//         return (n * (n - 1) / 2) - goodPairs;
+//     }
+// };
