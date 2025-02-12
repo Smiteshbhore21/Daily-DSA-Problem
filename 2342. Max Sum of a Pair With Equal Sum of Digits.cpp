14f1c662e56802356474a9f9b484a2770d096994
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-02-12 19:22:46 +0530

    Update 2342. Max Sum of a Pair With Equal Sum of Digits.cpp

--- 2342. Max Sum of a Pair With Equal Sum of Digits.cpp
+++ 2342. Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
+        // O(n) --> 
+        // int n = nums.size(), ans = -1;
+        // unordered_map<int, vector<int>> mp;
+        // for (int i = 0; i < n; i++) {
+        //     int digSum = countDigitSum(nums[i]);
+        //     mp[digSum].push_back(i);
+        // }
+        // for (auto& kv : mp) {
+        //     if (kv.second.size() > 1) {
+        //         vector<int> temp = kv.second;
+        //         int first = temp[0], second = temp[1];
+        //         if (nums[first] < nums[second]) {
+        //             swap(first, second);
+        //         }
+        //         for (int i = 2; i < temp.size(); i++) {
+        //             if (nums[temp[i]] > nums[first]) {
+        //                 second = first;
+        //                 first = temp[i];
+        //             } else if (nums[temp[i]] > nums[second]) {
+        //                 second = temp[i];
+        //             }
+        //         }
+        //         ans = max(ans, nums[first] + nums[second]);
+        //     }
+        // }
+        // return ans;
+
+        //O(N) -->
         int n = nums.size(), ans = -1;
-        unordered_map<int, vector<int>> mp;
+        vector<int> mp(82, 0);    //Here 82 taken cuz Constraints    1 <= nums[i] <= 10^9, so max possible digit sum will be 81 (999999999).
         for (int i = 0; i < n; i++) {
             int digSum = countDigitSum(nums[i]);
-            mp[digSum].push_back(i);
-        }
-        for (auto& kv : mp) {
-            if (kv.second.size() > 1) {
-                vector<int> temp = kv.second;
-                int first = temp[0], second = temp[1];
-                if (nums[first] < nums[second]) {
-                    swap(first, second);
-                }
-                for (int i = 2; i < temp.size(); i++) {
-                    if (nums[temp[i]] > nums[first]) {
-                        second = first;
-                        first = temp[i];
-                    } else if (nums[temp[i]] > nums[second]) {
-                        second = temp[i];
-                    }
-                }
-                ans = max(ans, nums[first] + nums[second]);
-            }
+            if (mp[digSum] > 0) {
+                int prevNum = mp[digSum];
+                ans = max(prevNum + nums[i], ans);
+                mp[digSum] = max(nums[i], prevNum);
+            } else
+                mp[digSum] = nums[i];
         }
         return ans;
     }
