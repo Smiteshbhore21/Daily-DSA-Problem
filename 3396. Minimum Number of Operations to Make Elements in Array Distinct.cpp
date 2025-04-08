3a1d4777da49f87229471b6f674a3d3b5f555c3c
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-04-08 22:53:59 +0530

    Update 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp

--- 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
+++ 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int, int> numCount;
-        set<int> dupli;
-        int n = nums.size(), ans = 0;
-        for (int& x : nums) {
-            numCount[x]++;
-            if (numCount[x] > 1)
-                dupli.insert(x);
+        for (int i = n - 1; i >= 0; i--) {
+            numCount[nums[i]]++;
+            if (numCount[nums[i]] > 1)
+                return ceil((i + 1) / 3.0);
         }
-        for (int i = 0; i < n - 2; i += 3) {
-            if (dupli.empty())
-                break;
-            ans++;
-            numCount[nums[i]]--, numCount[nums[i + 1]]--,
-                numCount[nums[i + 2]]--;
-            if (numCount[nums[i]] <= 1)
-                dupli.erase(nums[i]);
-
-            if (numCount[nums[i + 1]] <= 1)
-                dupli.erase(nums[i + 1]);
-
-            if (numCount[nums[i + 2]] <= 1)
-                dupli.erase(nums[i + 2]);
-        }
-        if (!dupli.empty())
-            ans++;
-        return ans;
+        return 0;
     }
+    // int minimumOperations(vector<int>& nums) {
+    //     unordered_map<int, int> numCount;
+    //     set<int> dupli;
+    //     int n = nums.size(), ans = 0;
+    //     for (int& x : nums) {
+    //         numCount[x]++;
+    //         if (numCount[x] > 1)
+    //             dupli.insert(x);
+    //     }
+    //     for (int i = 0; i < n - 2; i += 3) {
+    //         if (dupli.empty())
+    //             break;
+    //         ans++;
+    //         numCount[nums[i]]--, numCount[nums[i + 1]]--,
+    //             numCount[nums[i + 2]]--;
+    //         if (numCount[nums[i]] <= 1)
+    //             dupli.erase(nums[i]);
+
+    //         if (numCount[nums[i + 1]] <= 1)
+    //             dupli.erase(nums[i + 1]);
+
+    //         if (numCount[nums[i + 2]] <= 1)
+    //             dupli.erase(nums[i + 2]);
+    //     }
+    //     if (!dupli.empty())
+    //         ans++;
+    //     return ans;
+    // }
 };
