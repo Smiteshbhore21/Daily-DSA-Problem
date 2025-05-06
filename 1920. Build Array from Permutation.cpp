34129178c9beda3f992c784c2c7704d218334b6e
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-05-06 14:13:40 +0530

    Create 1920. Build Array from Permutation.cpp

---
+++ 1920. Build Array from Permutation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] + (1000 * (nums[nums[i]] % 1000));
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] /= 1000;
+        }
+        return nums;
+
+
+        // int n = nums.size();
+        // vector<int> ans(n);
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = nums[nums[i]];
+        // }
+        // return ans;
+    }
+};
