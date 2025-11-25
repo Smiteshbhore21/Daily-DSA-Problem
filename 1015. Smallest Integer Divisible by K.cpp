a6c52d89453fd663c878c7645852aefc9d7a4ba9
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-11-25 16:49:29 +0530

    Create 1015. Smallest Integer Divisible by K.cpp

---
+++ 1015. Smallest Integer Divisible by K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0)
+            return -1;
+        int rem = 0;
+        int digitCnt = 1;
+        while (digitCnt <= k) {
+            rem = ((rem * 10) + 1) % k;
+            if (rem == 0)
+                return digitCnt;
+            digitCnt++;
+        }
+        return -1;
+    }
+};
