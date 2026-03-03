15bc992dd739d61423d65a8bc7f72f30e7c95b97
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-03-03 16:11:40 +0530

    Update 1545. Find Kth Bit in Nth Binary String.cpp

--- 1545. Find Kth Bit in Nth Binary String.cpp
+++ 1545. Find Kth Bit in Nth Binary String.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        string s = formString(n, "0", 1);
-        cout << s << endl;
-        return s[k-1];
-    }
+        if (n == 1)
+            return '0';
 
-    string formString(const int& n, string currStr, int ops) {
-        if (ops == n)
-            return currStr;
-        string revert = "";
-        for (char ch : currStr) {
-            revert += ('1' == ch ? '0' : '1');
+        int length = (1 << n) - 1;
+
+        if (k < ceil(length / 2.0))
+            return findKthBit(n - 1, k);
+        else if (k == ceil(length / 2.0))
+            return '1';
+        else {
+            char ch = findKthBit(n - 1, length - (k - 1));
+            return ch == '0' ? '1' : '0';
         }
-        reverse(revert.begin(), revert.end());
-        string newStr = currStr + '1' + revert;
-        return formString(n, newStr, ops + 1);
     }
 };
+
+// class Solution {
+// public:
+//     unordered_map<string, string> memo;
+//     char findKthBit(int n, int k) {
+//         string s = formString(n, "0", 1);
+//         cout << s << endl;
+//         return s[k - 1];
+//     }
+//     string formString(const int& n, string currStr, int ops) {
+//         if (ops == n)
+//             return currStr;
+//         string key = currStr + "_" + to_string(ops);
+//         if (memo.find(key) != memo.end())
+//             return memo[key];
+//         string revert = "";
+//         for (char ch : currStr) {
+//             revert += ('1' == ch ? '0' : '1');
+//         }
+//         reverse(revert.begin(), revert.end());
+//         string newStr = currStr + '1' + revert;
+//         return memo[key] = formString(n, newStr, ops + 1);
+//     }
+// };
