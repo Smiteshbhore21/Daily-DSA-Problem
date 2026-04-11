125ac4b776356991fc9917b9d7f36293e1cc4414
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-04-11 12:55:34 +0530

    Create 3741. Minimum Distance Between Three Equal Elements II.cpp

---
+++ 3741. Minimum Distance Between Three Equal Elements II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+        int n = nums.size();
+        int result = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+            if (mp[nums[i]].size() >= 3) {
+                int k = mp[nums[i]].back();
+                int j = mp[nums[i]][mp[nums[i]].size() - 3];
+
+                result = min(result, (k - j));
+            }
+        }
+        return result == INT_MAX ? -1 : 2 * result;
+    }
+};
