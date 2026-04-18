bb28952ac1106ba3cbda4be38cc341e00dbf3842
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-04-18 11:29:06 +0530

    Create 3783. Mirror Distance of an Integer.cpp

---
+++ 3783. Mirror Distance of an Integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mirrorDistance(int n) { return abs(n - reverse(n)); }
+    int reverse(int n) {
+        int rev = 0;
+        while (n) {
+            rev = (rev * 10) + n % 10;
+            n /= 10;
+        }
+        return rev;
+    }
+};
