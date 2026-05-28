bf0ed44e59d7dcde92c161bc4794f20418aed94e
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-05-28 20:01:49 +0530

    Create 3093. Longest Common Suffix Queries.cpp

---
+++ 3093. Longest Common Suffix Queries.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    struct TrieNode {
+        TrieNode* children[26];
+        int idx;
+        ~TrieNode() {
+            for (int i = 0; i < 26; i++) {
+                delete children[i];
+            }
+        }
+    };
+
+    TrieNode* getNode(int i) {
+        TrieNode* temp = new TrieNode();
+        temp->idx = i;
+        for (int j = 0; j < 26; j++) {
+            temp->children[j] = NULL;
+        }
+        return temp;
+    }
+
+    void insertTrie(TrieNode* root, int i, vector<string>& wordsContainer) {
+        string& word = wordsContainer[i];
+
+        int n = word.length();
+
+        for (int j = n - 1; j >= 0; j--) {
+            char ch = word[j];
+            int ch_idx = ch - 'a';
+
+            if (root->children[ch_idx] == NULL) {
+                root->children[ch_idx] = getNode(i);
+            }
+            root = root->children[ch_idx];
+
+            if (wordsContainer[root->idx].length() > n) {
+                root->idx = i;
+            }
+        }
+    }
+
+    int searchTrie(TrieNode* root, string& word) {
+        int result_idx = root->idx;
+        int n = word.length();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int ch_idx = word[i] - 'a';
+
+            root = root->children[ch_idx];
+            if (root == NULL) {
+                return result_idx;
+            }
+            result_idx = root->idx;
+        }
+
+        return result_idx;
+    }
+
+    vector<int> stringIndices(vector<string>& wordsContainer,
+                              vector<string>& wordsQuery) {
+        int m = wordsContainer.size();
+        int n = wordsQuery.size();
+
+        vector<int> result(n);
+
+        TrieNode* root = getNode(0);
+
+        for (int i = 0; i < m; i++) {
+            int idx = root->idx;
+            if (wordsContainer[idx].length() > wordsContainer[i].length()) {
+                root->idx = i;
+            }
+            insertTrie(root, i, wordsContainer);
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = searchTrie(root, wordsQuery[i]);
+        }
+
+        delete root;
+
+        return result;
+    }
+};
