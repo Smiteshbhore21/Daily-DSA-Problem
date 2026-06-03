80c2425b28ca413dd942e60e17331e53f290445c
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-06-03 11:51:08 +0530

    Create 3635. Earliest Finish Time for Land and Water Rides II.cpp

---
+++ 3635. Earliest Finish Time for Land and Water Rides II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+        int bestLandFinish = INT_MAX;
+        int bestWaterFinish = INT_MAX;
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < landStartTime.size(); i++) {
+            bestLandFinish =
+                min(bestLandFinish, landStartTime[i] + landDuration[i]);
+        }
+
+        for (int i = 0; i < waterStartTime.size(); i++) {
+            int curr =
+                max(bestLandFinish, waterStartTime[i]) + waterDuration[i];
+            ans = min(ans, curr);
+        }
+
+        for (int i = 0; i < waterStartTime.size(); i++) {
+            bestWaterFinish =
+                min(bestWaterFinish, waterStartTime[i] + waterDuration[i]);
+        }
+
+        for (int i = 0; i < landStartTime.size(); i++) {
+            int curr = max(bestWaterFinish, landStartTime[i]) + landDuration[i];
+            ans = min(ans, curr);
+        }
+
+        return ans;
+    }
+};
