813292d573467a24734d3d7d14def45913aa3be8
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2026-07-15 20:49:15 +0530

    Create 3658. GCD of Odd and Even Sums.java

---
+++ 3658. GCD of Odd and Even Sums.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int sumEven = n * (n + 1);
+        int sumOdd = n * n;
+
+        return gcd(sumEven, sumOdd);
+    }
+
+    public int gcd(int a, int b) {
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
